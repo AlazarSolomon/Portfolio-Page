a3b1812886f4b2686073ab213b17dcc394e1c8b9
Alazar Gebremichael <alge0668@colorado.edu>
2022-04-16 21:58:18 -0600

    new page

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+//   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+//   var collapsibleElem = document.querySelector('.collapsible');
+//   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  var myCarousel = document.querySelector('#myCarousel')
+  var carousel = new bootstrap.Carousel(myCarousel)
